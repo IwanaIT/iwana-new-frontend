f856bdd6c918e19579f84dce9487128412b98ec1
Hernán Bozán <hernan@iwanacash.com>
2021-01-15 23:57:59 -0300

    Agregado DestacadoEn y ComoComprar

---
+++ src/components/layout/ComoComprar.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="promo-section ptb-100">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="section-heading text-center mb-5 ">
+            <h2 class="color-iwana-primary">¿Cómo usar IwanaCash?</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row equal">
+        <div class="col-md-4 col-lg-4">
+          <div class="single-promo single-promo-hover single-promo-1 rounded text-center white-bg p-5 h-100">
+            <div class="circle-icon mb-5">
+              <span class="ti-face-smile text-white"></span>
+            </div>
+            <h5>1. ÚNETE</h5>
+            <p>Primero lo primero: Te registras</p>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-4">
+          <div class="single-promo single-promo-hover single-promo-1 rounded text-center white-bg p-5 h-100">
+            <div class="circle-icon mb-5">
+              <span class="ti-shopping-cart-full text-white"></span>
+            </div>
+            <h5>2. COMPRA</h5>
+            <p>
+              Busca tu tienda:
+              Tenemos grandes
+              marcas
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 col-lg-4">
+          <div
+              class="single-promo single-promo-hover single-promo-1 rounded text-center white-bg p-5 h-100"
+          >
+            <div class="circle-icon mb-5">
+              <span class="ti-money text-white"></span>
+            </div>
+            <h5>3. OBTÉN CASHBACK</h5>
+            <p>
+              En unos días, tienes tu cashback en tu billetera
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ComoComprar"
+}
+</script>
+
+<style scoped>
+
+</style>
