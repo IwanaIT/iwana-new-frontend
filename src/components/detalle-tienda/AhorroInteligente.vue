4fc259eb6571192fda4deef92e5ba5478275e101
IwanaIT <desarrolloit@iwanacash.com>
2021-03-20 17:23:51 -0300

    Actualización para Tienda

---
+++ src/components/detalle-tienda/AhorroInteligente.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="single-blog-card card border-0 shadow-sm">
+    <div class="meta-date card-body">
+      <div class="row">
+        <div class="col-sm-12 col-md-2">
+          <img src="images/light.svg" class="img-fluid">
+        </div>
+        <div class="col-sm-12 col-md-10">
+          <h4>Ahorro Inteligente</h4>
+          <p class="lead">
+            ¡Ya comenzaste tu ruta por el camino del cashback! Sigue así y verás volver tu dinero de forma segura y directa a tu bolsillo, porque con Iwana solo sumas. Sé inteligente y aprovecha todas las ofertas que hemos preparado para ti
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "DetallePerfilTienda",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {
+
+  }
+}
+</script>
