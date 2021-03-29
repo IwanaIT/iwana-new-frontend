a3addc8a6b167eea90bd6bd90acd9171630e528a
IwanaIT <desarrolloit@iwanacash.com>
2021-03-28 22:35:59 -0300

    Actualización para Tienda

--- src/components/detalle-tienda/PerfilTienda.vue
+++ src/components/detalle-tienda/PerfilTienda.vue
@@ -2,13 +2,13 @@
   <div class="single-blog-card card border-0 shadow-sm text-center">
     <div class="meta-date card-body">
       <img
-          :src="'test/que-buscas-hoy.png'"
+          :src="logo"
           class="card-img-top position-relative p-3 img-fluid"
           alt="blog"
           style="max-width:70%;"
       />
       <h5 class="card-title">{{ nombreTienda }}</h5>
-      <h4 class="color-iwana-primary">Hasta {{ porcentajeCashback }} de Cashback</h4>
+      <h4 class="color-iwana-primary">Hasta {{ porcentajeCashback }}% de Cashback</h4>
       <br>
       <b-button variant="outline-info">Comprar Ahora</b-button>
     </div>
@@ -21,6 +21,7 @@
 export default {
   name: "PerfilTienda",
   props: [
+    'logo',
     'nombreTienda',
     'porcentajeCashback'
   ],
--- src/views/Tienda.vue
+++ src/views/Tienda.vue
@@ -6,12 +6,13 @@
         <div class="container">
           <div class="row">
             <div class="col-md-12">
-              <img src="test/banner-whole.jpg" class="img-fluid">
+              <img :src="bannerTienda" class="img-fluid" :alt="nombreTienda">
             </div>
           </div>
           <div class="row mt-4">
             <div class="col-sm-12 col-md-4">
               <perfil-tienda
+                  :logo="logo"
                   :nombre-tienda="nombreTienda"
                   :porcentaje-cashback="porcentajeCashback"
               />
@@ -44,6 +45,8 @@ import DetallePerfilTienda from "../components/detalle-tienda/DetallePerfilTiend
 import TerminosYCondiciones from "../components/detalle-tienda/TerminosYCondiciones";
 import Cashbacks from "../components/detalle-tienda/Cashbacks";
 import AhorroInteligente from "../components/detalle-tienda/AhorroInteligente";
+import axios from "axios";
+//import VLazyImage from "v-lazy-image";
 
 export default {
   name: "Tienda",
@@ -53,33 +56,54 @@ export default {
     DetallePerfilTienda,
     TerminosYCondiciones,
     Cashbacks,
-    AhorroInteligente
+    AhorroInteligente,
+    //VLazyImage
   },
   data() {
     return {
+      logo: '',
+      bannerTienda: '',
       nombreTienda: '',
       descripcion: '',
       tiposCashback: '',
       terminosYCondiciones: '',
       porcentajeCashback: '',
-      cashbacks: [
-        {'titulo': 'Todas las categorias', 'porcentaje': '6', 'link': '#'},
-        {'titulo': 'Todas las categorias', 'porcentaje': '6', 'link': '#'},
-        {'titulo': 'Todas las categorias', 'porcentaje': '6', 'link': '#'},
-      ]
+      cashbacks: []
     };
-  },
-  methods: {
-    getDataTienda() {
-      this.nombreTienda = 'Abcdin';
-      this.porcentajeCashback = '6%';
-      this.descripcion = '¡En Abcdin.cl sabemos de Tecnología! Y te la ofrecemos al mejor precio. Encuentra lo mejor en Smartphones, Notebooks, LED y Audio, Linea Blanca y mucho más. Despachamos tus compras a todo Chile o si prefieres, ¡retira tus compras gratis en tu tienda Abcdin más cercana!';
-      this.tiposCashback = '<h5>¿En Abcdin puedes obtener Cashback?</h5><p>¡Sí! En toda la tienda</p>';
-      this.terminosYCondiciones = 'Categorías: Todas.Tiempo promedio espera Cashback: 2 meses.'
-    },
   },
   mounted() {
     this.getDataTienda();
-  }
+  },
+  methods: {
+    getDataTienda() {
+      let codigoPais = this.$route.params.pais;
+      let nombreTienda = this.$route.params.nombreTienda;
+      let url = '/api/frontend/public/tiendas/' + codigoPais + '/tienda/' + nombreTienda;
+      axios
+          .get(url)
+          .then(response => {
+            this.bannerTienda = response.data.data.banner;
+            this.descripcion = response.data.data.descripcion;
+            this.nombreTienda = response.data.data.nombre;
+            this.terminosYCondiciones = response.data.data.terminosYCondiciones;
+            this.tiposCashback = response.data.data.tipoCashback;
+            this.logo = response.data.data.logo;
+            this.porcentajeCashback = response.data.data.maxCashback;
+
+            let cashbacksData = []
+            response.data.data.cashbacks.map(function (cashbacks) {
+              cashbacksData.push({
+                'id': cashbacks.id,
+                'porcentaje': cashbacks.cashback,
+                'link': cashbacks.link,
+                'titulo': cashbacks.nombre
+              })
+            });
+
+            this.cashbacks = cashbacksData;
+          })
+    }
+  },
 }
+
 </script>
